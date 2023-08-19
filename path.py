5e569acbc078139ae444245da949ab504de90dc5
ABHISEK GUHA <119780796+Abhisekguha@users.noreply.github.com>
2023-08-19 22:30:30 +0530

    Add files via upload

---
+++ path.py
@@ -0,0 +1,10 @@
+import os
+
+# Replace 'filename.txt' with the name of your file
+file_path = r'C:\Users\Acer\Desktop\bard\collab\data\know-india-booklet.pdf'
+
+
+# Get the directory of the file
+directory = os.path.dirname(file_path)
+
+print(directory)
